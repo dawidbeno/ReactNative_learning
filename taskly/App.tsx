8eb9a7473d0b6984e6da401a8e5d6853f1659728
David Beno <dawid.beno@gmail.com>
2025-08-07 10:00:10 +0200

    delete button with alert

--- taskly/App.tsx
+++ taskly/App.tsx
@@ -1,11 +1,35 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Alert } from 'react-native';
 import { theme } from "./theme";
 
 export default function App() {
+  const handleDelete = () => {
+    Alert.alert("Are you sure you want to delete this?",
+      "It will be gone for good",
+      [
+        {
+          text: "Yes",
+          onPress: () => console.log("Ok, deleting"),
+          style: "destructive",
+        },
+        {
+          text: "Cancel",
+          style: "cancel"
+        }
+      ],
+    );
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.itemContainer}>
         <Text style={styles.itemText}>Coffee</Text>
+        <TouchableOpacity 
+          onPress={handleDelete}
+          style={styles.button}
+          activeOpacity={0.8}
+          >
+          <Text style={styles.buttonText}>Delete</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -18,13 +42,27 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   itemContainer: {
-    paddingVertical: 16,
-    paddingHorizontal: 8,
-    borderBottomColor: theme.colorCerulean,
     borderBottomWidth: 1,
+    borderBottomColor: theme.colorCerulean,
+    paddingHorizontal: 8,
+    paddingVertical: 16,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
   },
   itemText: {
     fontSize: 18,
     fontWeight: "200",
   },
+  button: {
+    backgroundColor: theme.colorBlack,
+    padding: 8,
+    borderRadius: 6,
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    letterSpacing: 1,
+  }
 });
