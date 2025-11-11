f7ce4aff371efcecfa7bebdf404dd739be7bd48b
David Beno <dawid.beno@gmail.com>
2025-11-11 18:44:29 +0100

    new project with bottom tab navigation

---
+++ plantly/app/_layout.tsx
@@ -0,0 +1,31 @@
+import { Tabs } from "expo-router";
+import Entypo from "@expo/vector-icons/Entypo";
+import Feather from "@expo/vector-icons/Feather";
+import { theme } from "../theme";
+
+export default function Layout() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: theme.colorGreen }}>
+        <Tabs.Screen
+            name="index"
+            options={{
+                title: "Home",
+                tabBarShowLabel: false,
+                tabBarIcon: ({ color, size }) => (
+                    <Entypo name="home" color={color} size={size} />
+                ),
+            }}
+        />
+        <Tabs.Screen
+            name="profile"
+            options={{
+                title: "Profile",
+                tabBarShowLabel: false,
+                tabBarIcon: ({ color, size }) => (
+                    <Feather name="user" color={color} size={size} />
+                ),
+            }}
+        />  
+    </Tabs>
+  );
+}
