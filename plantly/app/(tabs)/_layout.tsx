db9513411bbec65f968ae358df43c8d7807e9fde
David Beno <dawid.beno@gmail.com>
2025-11-26 22:24:25 +0100

    new screen for add plant, modal presentation

--- plantly/app/(tabs)/_layout.tsx
+++ plantly/app/(tabs)/_layout.tsx
@@ -1,8 +1,10 @@
-import { Tabs, Redirect } from "expo-router";
+import { Tabs, Redirect, Link } from "expo-router";
 import Entypo from "@expo/vector-icons/Entypo";
 import Feather from "@expo/vector-icons/Feather";
 import { theme } from "@/theme";
 import { useUserStore } from "@/store/userStore";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import { Pressable } from "react-native";
 
 export default function Layout() {
     const hasFinishedOnboarding = useUserStore(
@@ -22,6 +24,17 @@ export default function Layout() {
                 tabBarShowLabel: false,
                 tabBarIcon: ({ color, size }) => (
                     <Entypo name="home" color={color} size={size} />
+                ),
+                headerRight: () => (
+                    <Link href="/new" asChild>
+                        <Pressable hitSlop={20} style={{ marginRight: 18 }}>
+                            <AntDesign 
+                                name="plus-circle"
+                                size={24}
+                                color={theme.colorGreen}
+                            />
+                        </Pressable>
+                    </Link>
                 ),
             }}
         />
