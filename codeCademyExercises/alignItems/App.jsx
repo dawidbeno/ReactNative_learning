f349994450fcc0fefd413d397b3a8aafe0d0df0d
David Beno <dawid.beno@gmail.com>
2026-01-14 19:47:32 +0100

    Align Items lesson

---
+++ codeCademyExercises/alignItems/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+const App = () => (
+  <View style={styles.layout}>
+    <View style={[styles.box, { backgroundColor: 'red' }]} />
+    <View style={[styles.box, { backgroundColor: 'green' }]} />
+    <View style={[styles.box, { backgroundColor: 'blue' }]} />
+  </View>
+);
+
+export default App;
+
+export const styles = StyleSheet.create({
+  layout: {
+    flex: 1,
+    backgroundColor: '#e5e5e5',
+  },
+  box: {
+    backgroundColor: 'black',
+    height: 100,
+    width: 100,
+  },
+});
+
+// Exerciese:
+// Add the alignItems property on the layout and set it to flex-end or center.
