65f2c24a033e3dd73d9f2a727bc6b0f52c0c6027
David Beno <dawid.beno@gmail.com>
2026-02-03 20:27:11 +0100

    new projects

---
+++ codeCademy/lab_navigation_fitness_tracker/App.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { NavigatorScreenParams } from '@react-navigation/native';
+import { SettingsScreen } from './components/SettingsScreen';
+import { ProfileScreen } from './components/ProfileScreen';
+import { GoalsScreen } from './components/GoalsScreen';
+import { ProgressScreen } from './components/ProgressScreen';
+import { WorkoutDetailScreen } from './components/WorkoutDetailScreen';
+import { WorkoutsScreen } from './components/WorkoutsScreen';
+
+
+export type WorkoutStackParamList = {
+  Workouts: undefined;
+  WorkoutDetail: { workoutName: string };
+};
+
+export type ProgressStackParamList = {
+  Progress: undefined;
+  Goals: undefined;
+};
+
+export type ProfileStackParamList = {
+  Profile: undefined;
+  Settings: undefined;
+};
+
+
+type RootTabParamList = {
+  WorkoutsTab: NavigatorScreenParams<WorkoutStackParamList>,
+  ProgressTab: NavigatorScreenParams<ProgressStackParamList>,
+  ProfileTab: NavigatorScreenParams<ProfileStackParamList>
+}
+
+// Main App
+const App = () => {
+  return (
+   <></>
+  );
+};
+
+
+export default App;
